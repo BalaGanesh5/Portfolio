3ae347ee6b0b0e220fac4aad93119c4fb381c704
BalaGanesh5 <balaganesh405@gmail.com>
2020-09-14 14:18:25 +0530

    Updates

---
+++ precache-manifest.13ef3e14bb62ff9109413614c3d89d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43100fe1da3ee8f9e08f0347443142ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0af7a67f33b10b35fc92",
+    "url": "/static/css/main.990651d7.chunk.css"
+  },
+  {
+    "revision": "689f54020d27b74c79b8",
+    "url": "/static/js/2.a03a2e62.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.a03a2e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af7a67f33b10b35fc92",
+    "url": "/static/js/main.3811b88c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
